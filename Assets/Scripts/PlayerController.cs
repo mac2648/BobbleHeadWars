372e6656294e85686009acab15b03b89694641dd
Marcelo vitor Toscan de Oliveira <marcelo1112@live.com>
2023-09-26 14:29:16 -0400

    Chapter 4 - Introducing character controllers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float MoveSpeed = 50.0f;
+    public float moveSpeed = 50.0f;
+    private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += MoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        pos.z += MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-
-        transform.position = pos;
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
     }
 }
