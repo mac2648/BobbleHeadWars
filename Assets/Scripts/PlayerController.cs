9c323803b95a78f63eb05c69adb7e48e649d1976
Marcelo vitor Toscan de Oliveira <marcelo1112@live.com>
2023-09-26 15:12:56 -0400

    Chapter 4 - Raycasting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     public Rigidbody head;
+    public LayerMask layerMask;
 
+    private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
     
     // Start is called before the first frame update
@@ -33,5 +35,27 @@ public class PlayerController : MonoBehaviour
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;  
+            }
+            // 1
+            Vector3 targetPosition = new Vector3(hit.point.x,
+             transform.position.y, hit.point.z);
+            // 2
+            Quaternion rotation = Quaternion.LookRotation(targetPosition -
+             transform.position);
+            // 3
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+             rotation, Time.deltaTime * 10.0f);
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
     }
 }
