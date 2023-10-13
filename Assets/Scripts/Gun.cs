8c879c38dc482796e9b3fa1dc5ef213fbade0910
Marcelo vitor Toscan de Oliveira <marcelo1112@live.com>
2023-10-13 15:39:08 -0400

    Chapter 7 - modified gun script to be upgradable

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,6 +7,10 @@ public class Gun : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform launchPosition;
 
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
+
     private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
@@ -33,14 +37,37 @@ public class Gun : MonoBehaviour
 
     void fireBullet()
     {
-        // 1
-        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
-        // 2
-        bullet.transform.position = launchPosition.position;
-        // 3
-        bullet.GetComponent<Rigidbody>().velocity =
-        transform.parent.forward * 100;
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
 
-        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        if (isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity = (transform.right + transform.forward / 0.5f) * 100;
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity = ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+
+        if (isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
+    }
+
+    private Rigidbody createBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
+    public void UpgradeGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
     }
 }
